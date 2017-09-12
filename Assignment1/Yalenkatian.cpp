5ad1515cfaf6014c871f99fbcc3a5e9eba181a39
Joseph Yalenkatian <yalenkat@unlv.nevada.edu>
2017-09-11 20:46:44 -0700

    Adding my Hello World CPP file

---
+++ Assignment1/Yalenkatian.cpp
@@ -0,0 +1,10 @@
+// Joseph Yalenkatian - CS 472
+// Git Assignment - Hello World Program
+
+#include <iostream>
+
+int main(){
+  std::cout << "Hello World! It's Joseph!" << std::endl;
+
+  return 0;
+}
